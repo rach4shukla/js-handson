18deace36c23920ef46648b32ce9565199bc2092
Rachna Shukla <144632061+rach4shukla@users.noreply.github.com>
2024-11-03 00:45:39 +0000

    variables lesson

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144;
+let accountEmail = "rachna@gmail.com";
+var accountPassword = "123rach";
+
+accountCity = "Cambridge";
+
+// accountId = 2;  Single line Comment
+accountEmail = "prashant@gmail.com";
+accountPassword = "456kashu";
+accountCity = "Lucknow";
+
+let accountState;
+
+/* 
+const ==> cannot be changed
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
