6e89269c977167ae3021245305970dd349459bf9
Rachna Shukla <144632061+rach4shukla@users.noreply.github.com>
2024-11-03 01:08:24 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3 + 3); //we are using nodejs, not browser
+
+console.log(3 
+    + 3); //code readability should be high, this is not a good practice
+
+console.log('Rachna');
+
+//PRIMITIVE DATATYPES
+
+let name = "Rachna"; //string ("" or '')
+let age = 33 //number (2 to power of 53, above then bigint is used usually in trading or stock market)
+ let isLoggedIn = false; //boolean ==> true/false
+
+ //null ==> datatype and a standalone value
+ //undefined ==> when no value is defined
+
+ let state; //undefined
+ let temp = ""; //null
+
+ //symbol ==> used in react for uniqueness
+
+// OBJECTS (MUST BE DISCUSSED IN DETAIL)
+
+console.log(typeof "Rachna");
+console.log(typeof null); // null is an object
+
+console.log(typeof undefined); //undefined type
+
+
