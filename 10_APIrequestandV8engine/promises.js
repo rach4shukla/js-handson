56f27a0e21c5fc1ec1f373535bb372ed3d9f0962
Rachna Shukla <144632061+rach4shukla@users.noreply.github.com>
2025-01-04 18:51:58 +0000

    promises (how to create one and consume one using then, catch and finally

--- 10_APIrequestandV8engine/promises.js
+++ 10_APIrequestandV8engine/promises.js
@@ -57,5 +57,51 @@ promiseFour.then(function(user){
     console.log(username);
 }).catch(function(error){
     console.log(error);
-})
+}).finally(() => console.log("The promise is either resolved or rejected"))
 
+const promiseFive = new Promise (function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "javascript", password: "123"})
+        } else{
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+
+// can be handled using async await (however it does not directly handle error)
+// we can handle it using try and catch block
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    } catch (error){
+        console.log(error);
+    }
+   
+}
+
+consumePromiseFive()
+
+async function getAllUsers(){
+    try{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        console.log(response);
+        const data = await response.json() // This takes time to convert so we need to async await it
+        console.log(data);
+    } catch(error){
+        console.log("E: ", error);
+        
+    }
+}
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+}).then((data) => {
+    console.log(data);
+    
+}).catch((error) => console.log(error))
